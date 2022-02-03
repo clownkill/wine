31e025cd6bdff4b66df3a68203e6ee000596bf48
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-02-03 16:41:24 +0300

    Render wine by categories

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
+from collections import defaultdict
 import datetime as dt
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+
+def get_age():
+    age = dt.datetime.now().year - 1920
+    return age
+
+
+def get_wine_by_categories(database):
+    wines = pandas.read_excel(
+        database,
+        na_values=False,
+        keep_default_na=False
+    ).to_dict(orient='records')
+    wine_by_categories = defaultdict(list)
+    for wine in wines:
+        wine_by_categories[wine['Категория']].append(wine)
+    return wine_by_categories
+
+
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
+database = 'wine.xlsx'
+
 template = env.get_template('template.html')
 
-age = dt.datetime.now().year - 1920
-wines = pandas.read_excel('wine.xlsx').to_dict(orient='records')
-
 render_page = template.render(
-    wine_factory_age=age,
-    wines=wines,
+    wine_factory_age=get_age(),
+    wines_by_category=get_wine_by_categories(database),
 )
 
 with open('index.html', 'w', encoding='utf8') as file:
